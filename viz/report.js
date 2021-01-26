a6d53c23eca49725f7ed54d1afee870ac0d2a3f1
Prateek Saxena <prtksxna@gmail.com>
2021-01-26 10:05:13 +0530

    refactor: indentation fixes

--- viz/report.js
+++ viz/report.js
@@ -5,21 +5,21 @@ $(function () {
 })
 
 langCoverage = function (data) {
-    var tally = [];
-    $.each(data, function (n, v) {
-      tally.push({
-        language: n,
-        noOfArticles: v.length
-      })
-    });
-
-    tally.sort( function(a, b){
-      return b.noOfArticles - a.noOfArticles;
-    });
-
-    tally.forEach(function (lang) {
-      var $el = $('<div>').text(lang.language + lang.noOfArticles);
-      $el.css('opacity',lang.noOfArticles / 10);
-      $('.js-vital10-langCoverage').append($el);
+  var tally = [];
+  $.each(data, function (n, v) {
+    tally.push({
+      language: n,
+      noOfArticles: v.length
     })
+  });
+
+  tally.sort( function(a, b){
+    return b.noOfArticles - a.noOfArticles;
+  });
+
+  tally.forEach(function (lang) {
+    var $el = $('<div>').text(lang.language + lang.noOfArticles);
+    $el.css('opacity',lang.noOfArticles / 10);
+    $('.js-vital10-langCoverage').append($el);
+  })
 }
