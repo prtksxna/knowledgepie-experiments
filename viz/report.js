06271d3c4f542f6c2a7de6e4d7451c453252237e
Prateek Saxena <prtksxna@gmail.com>
2021-01-26 09:58:15 +0530

    feat: add languageCoverage viz

--- viz/report.js
+++ viz/report.js
@@ -1,6 +1,25 @@
 $(function () {
-  alert(1);
   $.getJSON('/data_out/vital10.lang.json', function (data) {
-    console.log(data);
+    langCoverage(data.languageDetails);
   })
 })
+
+langCoverage = function (data) {
+    var tally = [];
+    $.each(data, function (n, v) {
+      tally.push({
+        language: n,
+        noOfArticles: v.length
+      })
+    });
+
+    tally.sort( function(a, b){
+      return b.noOfArticles - a.noOfArticles;
+    });
+
+    tally.forEach(function (lang) {
+      var $el = $('<div>').text(lang.language + lang.noOfArticles);
+      $el.css('opacity',lang.noOfArticles / 10);
+      $('.js-vital10-langCoverage').append($el);
+    })
+}
